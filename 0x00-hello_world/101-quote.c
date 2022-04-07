addf6cc609dc057eca083375c264331515e62aa0
rsannan <shawnrannan@gmail.com>
2022-04-07 14:20:43 -0700

    Fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,17 @@
 /**
 * main - Entry point
 *
-* Return: Always 2
+* Return: Always 1
  */
 
 int main(void)
 {
+int fail = 2;
+if (fail == 2)
+{
 fwrite("and that piece of art is useful\"", 33, 1, stderr);
 fwrite("- Dora Korpar, 2015-10-19\n", 27, 1, stderr);
-
-return (2);
+return (1);
+}
+return (0);
 }
