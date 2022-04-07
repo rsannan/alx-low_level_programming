765f359b246760f0d3be9c34b43acbce99b947fe
rsannan <shawnrannan@gmail.com>
2022-04-07 09:20:48 -0700

    Add Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+ */
+int main(void)
+{
+Printf("Size of a char: %d\n", sizeof(char));
+Printf("Size of an int: %d\n", sizeof(int));
+Printf("Size of a long int: %d\n", sizeof(long int));
+Printf("Size of a long long int: %d\n", sizeof(long long int));
+Printf("Size of a float: %d\n", sizeof(float));
+
+return(0);
+}
