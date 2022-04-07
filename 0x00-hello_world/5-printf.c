1e25f2655dc7e74fe31bdaac0144445a59caa758
rsannan <shawnrannan@gmail.com>
2022-04-07 08:55:30 -0700

    Add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+ */
+int main(void)
+{
+char ch[] = "with proper grammar, but the outcome is a piece of art,";
+
+printf("%s\n", ch);
+return(0)
+}
