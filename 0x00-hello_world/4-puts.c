56af4a0383e4a038cee8942fedb3d1d8b748a462
rsannan <shawnrannan@gmail.com>
2022-04-07 08:47:14 -0700

    Fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char ch[] = "\"Programming is like building a multilingual puzzle";	
+char ch[] = "\"Programming is like building a multilingual puzzle";
 
 puts(ch);
 return (0);
