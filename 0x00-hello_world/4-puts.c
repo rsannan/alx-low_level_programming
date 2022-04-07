30163a33e54ecc26e02ea8cb7e1848c982b9db03
rsannan <shawnrannan@gmail.com>
2022-04-07 08:39:41 -0700

    Fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
+ *Return: Always 0
  */
-int main (void)
+int main(void)
 {
-	char ch[] = "Programming is like building a multilingual puzzle";
+	char ch[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(ch);
 
 	return (0);
