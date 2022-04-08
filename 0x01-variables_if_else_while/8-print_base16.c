153a42ee88bb368f36f3c5bed4d44d0e02682a73
rsannan <shawnrannan@gmail.com>
2022-04-08 09:37:09 -0700

    Fix task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,11 @@ int main(void)
 int num;
 char ch;
 
-for (num = '0'; num <= 9; num++)
+for (num = 0; num <= 9; num++)
 {
 	putchar(num + '0');
 }
-for (ch = 'a'; ch < 'g'; ch++ )
+for (ch = 'a'; ch < 'g'; ch++)
 {
 	putchar(ch);
 }
