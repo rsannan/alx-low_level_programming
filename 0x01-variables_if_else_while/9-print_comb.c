27682377255c162405d51dd1be2852c2a01e644e
rsannan <shawnrannan@gmail.com>
2022-04-08 10:47:45 -0700

    Fix task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,8 @@ int num;
 for (num = 0; num <= 9; num++)
 {
 	putchar(num + '0');
-	for (num = 1; num <= 9; num++)
-	{
-		putchar(num + '0');
-		putchar(',' +' ');
-	}
+	putchar(',');
+	putchar(' ');
 }
 putchar('\n');
 
