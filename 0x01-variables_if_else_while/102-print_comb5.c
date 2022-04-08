edce4866dc6aa96c3974d1eaab46824c3d7267d4
rsannan <shawnrannan@gmail.com>
2022-04-08 15:52:59 -0700

    Fix task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,14 +20,12 @@ for (num1 = 0; num1 < 10; num1++)
 		{
 			for (num4 = 0; num4 < 10; num4++)
 			{
-				if (num4 > num2)
-				{
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(' ');
-					putchar(num3 + '0');
-					putchar(num4 + '0');
-					if (!((num2 == 8) && (num4 == 9)))
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(' ');
+				putchar(num3 + '0');
+				putchar(num4 + '0');
+				if (!((num1 == 9) && (num2 == 9) && (num3 == 9) && (num4 == 9)))
 					{
 						putchar(',');
 						putchar(' ');
