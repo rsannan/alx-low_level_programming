d92252e8a512c1f8e3973479aef0fc84ea05f665
rsannan <shawnrannan@gmail.com>
2022-04-08 09:33:02 -0700

    Add task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio>
+/**
+* main - Prints hexidecimal numbers
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int num;
+char ch;
+
+for (num = '0'; num <= 9; num++)
+{
+	putchar(num + '0');
+}
+for (ch = 'a'; ch < 'g'; ch++ )
+{
+	putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
