7cf42fb84a1ad8f7ffcd4742883e98efb355829b
rsannan <shawnrannan@gmail.com>
2022-04-08 09:50:54 -0700

    Fix task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main -
+* main - prints all combination of numbers
 *
-* Return:
+* Return: Always 0
 */
 
 int main(void)
@@ -12,7 +12,7 @@ int num;
 for (num = 0; num <= 9; num++)
 {
 	putchar(num + '0');
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; ++num)
 	{
 		putchar(num + '0');
 		putchar(',');
