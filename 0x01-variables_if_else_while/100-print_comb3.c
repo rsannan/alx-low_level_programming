88e03a30d00888d79c853481f010f9d97558d746
rsannan <shawnrannan@gmail.com>
2022-04-08 12:10:40 -0700

    Fix task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 int num;
+int num1;
 
 for (num = 0; num <= 9; num++)
 {
 	putchar(num + '0');
-	for (num = 1; num <= 9; num++)
+	for (num1 = 1; num1 <= 9; num1++)
 	{
-		putchar(num + '0');
+		putchar(num1 + '0');
 		putchar(',');
 		putchar(' ');
 	}
