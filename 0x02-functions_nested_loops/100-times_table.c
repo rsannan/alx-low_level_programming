55ae6b0be6a45868da95592741dcc60c6457576b
rsannan <shawnrannan@gmail.com>
2022-04-12 23:52:24 +0000

    Add task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+* print_times_table - prints n times table
+* @n: times table
+*
+* Return: Always void
+*/
+
+void print_times_table(int n)
+{
+int row;
+int column;
+int tens;
+int ones;
+int product;
+
+for (column = 0; column <= n; column++)
+{
+	for (row = 0; row <= n; row++)
+	{
+		product = row * n;
+		tens = product / 10;
+		ones = product % 10;
+		if (if column == 0)
+		{
+			_putchar(0);
+		}
+		else if (product < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(ones + '0');
+		}
+else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(tens + '0');
+			_putchar(ones + '0');
+		}
+	}
+}
+return;
+}
