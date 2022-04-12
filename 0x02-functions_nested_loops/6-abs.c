ba8277741a17d063f21234b0ce246aaa6b115e6b
rsannan <shawnrannan@gmail.com>
2022-04-12 07:57:41 -0700

    Added task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _abs - get absolute value of int
+* @int: number to be absoluted
+*
+* Return: Always result
+*/
+
+int _abs(int)
+{
+int result;
+
+if (int < 0)
+{
+	result = -(int);
+
+	return (result);
+}
+else
+{
+	result = int;
+
+	return (result);
+}
+}
