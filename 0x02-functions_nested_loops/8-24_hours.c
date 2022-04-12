026d44ca1fef2c75bdea0113dce54d70d46f23b2
rsannan <shawnrannan@gmail.com>
2022-04-12 12:32:48 -0700

    Fix task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,17 +20,14 @@ for (a = 0; a < 3; a++)
 		{
 			for (d = 0; d < 10; d++)
 			{
-				if ((a + b) <= 5)
+				if (((a * 10) + b) < 24)
 				{
-					if ((a + b + c + d) <= 19)
-					{
 						_putchar(a + '0');
 						_putchar(b + '0');
 						_putchar(':');
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
-					}
 				}
 			}
 		}
