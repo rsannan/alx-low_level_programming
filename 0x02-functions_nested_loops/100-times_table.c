9645bf805076f3c2026c2a68ac1ded68e0b06ea0
rsannan <shawnrannan@gmail.com>
2022-04-13 00:29:29 +0000

    Fix task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,6 +13,7 @@ int column;
 int tens;
 int ones;
 int product;
+int hundreds;
 
 for (row = 0; row <= n; row++)
 {
@@ -23,6 +24,7 @@ for (row = 0; row <= n; row++)
 			product = row * column;
 			tens = product / 10;
 			ones = product % 10;
+			hundreds = (product / 10) / 10
 			if (column == 0)
 			{
 				_putchar(0);
@@ -35,12 +37,22 @@ for (row = 0; row <= n; row++)
 				_putchar(' ');
 				_putchar(ones + '0');
 			}
+			else if (product < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(hundreds + '0');
 				_putchar(tens + '0');
 				_putchar(ones + '0');
 			}
