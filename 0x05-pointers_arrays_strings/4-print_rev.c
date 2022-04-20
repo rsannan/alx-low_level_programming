b455127079259c8e55174d46e154fe0c84c7bd74
rsannan <shawnrannan@gmail.com>
2022-04-20 10:11:28 -0700

    Add task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* print_rev - prints string in reverse
+* @s: string pointer
+*
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+len = _strlen(s);
+
+while (len >= 0)
+{
+	s = s + len;
+	_putchar(*s);
+	len--;
+}
+_putchar('\n');
+
+return;
+}
+
+/**
+* _strlen - gets length of a string
+* @s: string
+*
+* Return: always len
+*/
+int _strlen(char *s)
+{
+len = 0;
+
+while (*s != '\0')
+{
+	len++;
+	s++;
+}
+
+return (len);
+}
