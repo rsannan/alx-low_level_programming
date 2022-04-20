c2218e552fbf748e5cde08e5cbe72c29910d4c1d
rsannan <shawnrannan@gmail.com>
2022-04-20 18:10:13 +0000

    Fiz task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,13 @@
 void print_rev(char *s)
 {
 int len = _strlen(s);
-s = s + len;
-while (len >= 0)
+if (len != 0)
 {
-	_putchar(*s);
-	len--;
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
 }
 _putchar('\n');
 
