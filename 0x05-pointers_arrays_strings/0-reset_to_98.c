cfca8b5620671fbbb8eb052818d437ca923ed271
rsannan <shawnrannan@gmail.com>
2022-04-20 07:20:48 -0700

    Added directoru 05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* reset_to_98 - resets pointer value to 98
+* @n: pointer variable
+*
+* Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+
+return(0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# This repo shows how to use pointers, arrays and strings
