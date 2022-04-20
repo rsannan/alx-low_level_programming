9a8e01073067980e77e4790a460b4db28e47943c
rsannan <shawnrannan@gmail.com>
2022-04-20 20:51:33 +0000

    Add task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* puts2 - prints odd characters in string
+* @str: string pointer
+*
+* Return: void
+*/
+
+void puts2(char *str)
+{
+int i = 0;
+
+while (*str != '\0')
+{
+	if (i % 2 != 0)
+	{
+		_putchar(*str);
+		str++;
+	}
+	i++;
+}
+_putchar('\n');
+
+return;
+}
