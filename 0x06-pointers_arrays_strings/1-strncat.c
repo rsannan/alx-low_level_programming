16b4d3e743c3f5fc252d09479ef4bdeaf7226688
rsannan <shawnrannan@gmail.com>
2022-04-22 03:41:45 -0700

    Add task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+* _strncat - Adds one string to another string
+* @dest: string 1
+* @src: string 2
+* @n: number of bytes to be used
+*
+* Return: Pointer dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int lendest, lensrc, i;
+
+lendest = _strlen(dest);
+lensrc = _strlen(src);
+
+for (i = 0; i < (lensrc + 1); i++)
+{
+	if (i <= n)
+	{
+		*(dest + lendest) = *(src + i);
+	}
+}
+
+return (dest);
+}
+
+/**
+* _strlen - gets length of a string
+* @s: string
+*
+* Return: always len
+*/
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+	len++;
+	s++;
+}
+
+return (len);
+}
