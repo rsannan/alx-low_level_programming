0eb0bd6b412497842350a1b9778a298c3c74596c
rsannan <shawnrannan@gmail.com>
2022-04-23 01:57:41 +0000

    Fix task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,34 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i;
-int len = _strlen(dest);
+int i = 0, j= 0;
 
-while (*(src + i) != '\0')
+while (*(dest + i) != '\0')
+}
+	i++;
+}
+
+while (*(src + j) != '\0')
 {
-	*(dest + len) = *(src + i);
+	*(dest + i) = *(src + j);
+	j++;
 	i++;
 }
 *(dest + i) = '\0';
 
 return (dest);
-}
-
-/**
-* _strlen - gets length of a string
-* @s: string
-*
-* Return: always len
-*/
-
-int _strlen(char *s)
-{
-int len = 0;
-while (*s != '\0')
-{
-	len++;
-	s++;
-}
-
-return (len);
 }
