c6f2515eec9fec966b67cba020c4d627df029770
rsannan <shawnrannan@gmail.com>
2022-04-23 16:11:31 +0000

    Add task 6 complete

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
+* cap_string - capitalizes first letter of every word
+* @ch: string pointer
 *
+* Return: ch
 */
 
 char *cap_string(char *ch)
@@ -11,19 +14,26 @@ while (*(ch + i) != '\0')
 {
 	switch (ch[i])
 	{
-	case ' ' :
+	case ' ':
+	case '	':
+	case '\n':
+	case ',':
+	case ';':
+	case '.':
+	case '!':
+	case '?':
+	case '"':
+	case '(':
+	case ')':
+	case '{':
+	case '}':
 		if (ch[i + 1] <= 122 && ch[i + 1] >= 97)
 		{
 			ch[i + 1] = ch[i + 1] - 32;
 			break;
 		}
-	case '	' :
-		if (ch[i + 1] <= 122 && ch[i + 1] >= 97)
-{
-			ch[i + 1] = ch[i + 1] - 32;
-			break;
-}
 	}
 	i++;
 }
+return (ch);
 }
