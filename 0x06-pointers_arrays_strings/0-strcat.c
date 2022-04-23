a553d2f1aca746a6d2b2d175deae2dd9973b4c90
rsannan <shawnrannan@gmail.com>
2022-04-23 02:02:26 +0000

    Fix task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,18 @@ char *_strcat(char *dest, char *src)
 int i = 0;
 int j = 0;
 
-while (*(dest + i) != '\0')
+while (dest[i] != '\0')
 }
 	i++;
 }
 
-while (*(src + j) != '\0')
+while (src[j] != '\0')
 {
-	*(dest + i) = *(src + j);
+	dest[i] = src[j];
 	j++;
 	i++;
 }
-*(dest + i) = '\0';
+dest[i] = '\0';
 
 return (dest);
 }
