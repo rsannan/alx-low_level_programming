648cadaebbff8894badf0924d99b8a402f1c4cda
rsannan <shawnrannan@gmail.com>
2022-04-24 22:07:09 +0000

    Add task 10 - 12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,5 @@ int _strcmp(char *s1, char *s2);
 char *leet(char *ch);
 char *rot13(char *ch);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
