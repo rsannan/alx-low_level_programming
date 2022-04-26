95984c2b039815e5192304defc2e5a159acf1dc1
rsannan <shawnrannan@gmail.com>
2022-04-26 14:44:35 +0000

    Fix task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,7 +11,7 @@ char *_strchr(char *s, char c)
 {
 unsigned int i = 0;
 
-while (s[i] != '\0')
+while (s[i] != '\0' && )
 {
 	if (s[i] == c)
 	{
@@ -19,5 +19,5 @@ while (s[i] != '\0')
 	}
 	i++;
 }
-return (&s[i + 1]);
+return ((s + i));
 }
