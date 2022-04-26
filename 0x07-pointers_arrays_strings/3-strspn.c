1c9a06a03a67ad231313292623b33ae55e39108b
rsannan <shawnrannan@gmail.com>
2022-04-26 15:51:48 +0000

    Add task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,10 +3,24 @@
 * _strspn -Returns the number of bytes in the initial segment of s
 * @s: character pointer
 * @accept: character pointer
-* Return: s
+* Return: i
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
+unsigned int i;
 
+while (s[i] != '\0')
+{
+	if (s[i] == accept[i])
+	{
+		i++;
+	}
+	else
+	{
+		break;
+	}
+}
+
+return (i);
 }
