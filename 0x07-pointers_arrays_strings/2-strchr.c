d89a50203f9a80fbfa1afff3b036d9bd6dd66148
rsannan <shawnrannan@gmail.com>
2022-04-26 13:05:44 +0000

    Add task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strchr - prints first occurance of c
+* @s: string pointer
+* @c: character
+*
+* Return: s
+*/
+
+char *_strchr(char *s, char c)
+{
+unsigned int i = 0;
+
+while (s[i] != '\0')
+{
+	if (s[i] == c)
+	{
+		return ((s + i));
+	}
+	i++;
+}
+return (NULL);
+}
