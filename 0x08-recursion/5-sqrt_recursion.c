60fc1c33452260e288e818580513d990318c7ab5
rsannan <shawnrannan@gmail.com>
2022-04-28 19:26:03 +0000

    Add task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+* sqrt_sub - subtracts x to find perfect square of x
+* @x: number
+* @y: odd subtracter
+*
+* Return: sum
+*/
+int sqrt_sub(int x, int y)
+{
+int sum;
+
+if (x == 0)
+{
+	return (0);
+}
+else if (x < 0)
+{
+	return (-1);
+}
+else
+{
+	y = y + 2;
+	x = x - y;
+	sum = 1 + sqrt_sub(x, y);
+}
+return (sum);
+}
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: number
+*
+* Return: x if perfect square or -1 otherwise
+*/
+
+int _sqrt_recursion(int n)
+{
+int x;
+
+x = sqrt_sub(n, -1);
+
+if ((x ^ 2) == n)
+{
+	return (x);
+}
+else
+{
+	return (-1);
+}
+}
