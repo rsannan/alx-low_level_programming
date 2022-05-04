7055889bdd160503964276bc2ceaf04b31570cf7
rsannan <shawnrannan@gmail.com>
2022-05-04 12:58:30 +0000

    Finish task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,47 @@
 #include "main.h"
 /**
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
+ */
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
+}
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the string to be comverted.
+ *
+ *Return: The integer value of the comverted string.
+ */
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+
+	do {
+
+		if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
+/**
 * main - prints sum of positive numbers
 * @argc: argument count
 * @argv: argument string
@@ -8,7 +50,7 @@
 */
 int main(int argc, char **argv)
 {
-int i, sum;
+int i, sum = 0;
 if (argc < 2)
 {
 	_putchar('0');
@@ -16,16 +58,25 @@ if (argc < 2)
 }
 for (i = 0; i < argc; i++)
 {
-	if (!(argv[i] <= 57 && argv[i] >= 48))
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		return (1);
-	}
+	int j = 0;
 
+	while (argv[i][j] != '\0')
+	{
+		if (!(argv[i][j] <= 57 && argv[i] >= 48))
+		{
+			_putchar('E');
+			_putchar('r');
+			_putchar('r');
+			_putchar('o');
+			_putchar('r');
+			_putchar('\n');
+			return (1);
+		}
+		j++;
+	}
+	sum = sum + _atoi(argv[i]);
 }
+print_number(sum);
+_putchar('\n');
+return (0);
 }
