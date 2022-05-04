453115465a9dc2a488311329477401c16dd96ca9
rsannan <shawnrannan@gmail.com>
2022-05-04 14:29:02 +0000

    Fix task 0

--- 0x09-static_libraries/strcpy.c
+++ 0x09-static_libraries/strcpy.c
@@ -9,40 +9,8 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int len = _strlen(src);
-char ch[500];
-int i, j;
+(void)dest;
+(void)src;
 
-if (len != 0)
-{
-	for (i = 0; i <= len; i++)
-	{
-		ch[i] = *(src + i);
-	}
-
-	for (j = 0; j <= len; j++)
-	{
-		*(dest + j) = ch[j];
-	}
-}
 return (dest);
-}
-
-/**
-* _strlen - gets length of a string
-* @s: string
-*
-* Return: always len
-*/
-int _strlen(char *s)
-{
-int len = 0;
-
-while (*s != '\0')
-{
-	len++;
-	s++;
-}
-
-return (len);
 }
