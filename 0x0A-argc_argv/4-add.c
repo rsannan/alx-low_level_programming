3d62aa8faf72d7121fa333581f695f9a6c692206
rsannan <shawnrannan@gmail.com>
2022-05-04 05:28:11 -0700

    update task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,31 @@
 #include "main.h"
 /**
 * main - prints sum of positive numbers
+* @argc: argument count
+* @argv: argument string
+*
+* Return: o if error otherwise 1
 */
 int main(int argc, char **argv)
 {
+int i, sum;
+if (argc < 2)
+{
+	_putchar('0');
+	_putchar('\n');
+}
+for (i = 0; i < argc; i++)
+{
+	if (!(argv[i] <= 57 && argv[i] >= 48))
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		return (1);
+	}
 
+}
 }
