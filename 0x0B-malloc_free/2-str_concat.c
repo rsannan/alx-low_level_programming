f734d2c3d1d9940515919d7a2dab92e8229ff6e7
rsannan <shawnrannan@gmail.com>
2022-05-05 12:02:51 +0000

    Add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* strlenn - gets length of string
+* @s: string
+*
+* Return: int i
+*/
+int strlenn(char *s)
+{
+int i = 0;
+while (*s)
+{
+	i++;
+	s++;
+}
+return (i);
+}
+
+/**
+* str_concat - joins two strings in a third string
+* @s1: first string
+* @s2: second string
+*
+* Return: s if successful NULL otherwise
+*/
+char *str_concat(char *s1, char *s2)
+{
+int lens1, lens2, i = 0, j = 0;
+char *s;
+
+if (s1 == NULL)
+{
+	lens1 = 0;
+	lens2 = strlenn(s2);
+}
+else if (s2 == NULL)
+{
+	lens1 = strlenn(s1);
+	lens2 = 0;
+}
+else
+{
+	lens1 = strlenn(s1);
+	lens2 = strlenn(s2);
+}
+
+s = (char *)malloc(sizeof(char) * ((lens1 + lens2) - 1));
+while (i < lens1)
+{
+	s[i] = s1[i];
+	i++;
+}
+while (j < lens2)
+{
+	s[i] = s2[j];
+	j++;
+	i++;
+}
+s[i] = '\0';
+return (s);
+}
