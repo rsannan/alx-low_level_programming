3c138b078b96c3283c88b04f4d4850cae0cd94b9
rsannan <shawnrannan@gmail.com>
2022-05-06 12:58:39 +0000

    Add task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* strlenn - gets length of string
+* @s: string
+*
+* Return: int i
+*/
+int strlenn(char *s)
+{
+int i = 0;
+while (*s)
+{
+	i++;
+	s++;
+}
+return (i);
+}
+/**
+* argstostr - concatenates all the arguments of your program
+* @ac: argument count
+* @av: argument string
+*
+* Return: s
+*/
+char *argstostr(int ac, char **av)
+{
+int i, l, j = 0, tlen = 0, slen;
+char *s;
+
+if (ac == 0 || av == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+	tlen = tlen + strlenn(av[i]);
+}
+s = (char *)malloc(sizeof(char) * tlen);
+if (s == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+	l = 0;
+	slen = strlenn(av[i]);
+	while (l < slen)
+	{
+		s[j] = av[i][l];
+		l++;
+		j++;
+	}
+	_putchar('\n');
+}
+return (s);
+}
