231d77e7bbc4ac022b3808a7f7b93e2826ee4813
rsannan <shawnrannan@gmail.com>
2022-05-06 13:24:07 +0000

    Fix task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,7 +27,7 @@ return (i);
 char *argstostr(int ac, char **av)
 {
 int i;
-unsigned int  l, j = 0, tlen = 0, slen;
+unsigned int  l, j = 0, tlen = ac, slen;
 char *s;
 
 if (ac == 0 || av == NULL)
@@ -38,7 +38,7 @@ for (i = 0; i < ac; i++)
 {
 	tlen = tlen + strlenn(av[i]);
 }
-s = (char *)malloc(sizeof(char) * (tlen + 10));
+s = (char *)malloc(sizeof(char) * (tlen + 1));
 if (s == NULL)
 {
 	return (NULL);
