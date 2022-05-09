5d5f894b43417c0372788e2ddd164cee7967b57d
rsannan <shawnrannan@gmail.com>
2022-05-09 21:03:40 +0000

    Fix task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,18 +15,10 @@ int i, num, size;
 
 if (min > max)
 	return (NULL);
-if (max == min)
-	size = 1;
-
-else if (max < 0 && min < 0)
-	size = (-min - (-max)) + 1;
-
-else if (min < 0)
-	size = (max + (-min)) + 1;
-
 else
 	size = (max - min) + 1;
-s = malloc(sizeof(int) * (size + 1));
+
+s = malloc(sizeof(int) * size);
 
 if (s == NULL)
 	return (NULL);
@@ -40,7 +32,7 @@ else
 	for (i = 0; i < size; i++)
 	{
 		s[i] = num;
-		num = num + 1;
+		num++;
 	}
 }
 return (s);
