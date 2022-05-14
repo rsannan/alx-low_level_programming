d639e4ded045d0cb0e57c9b35277329398a56fa8
rsannan <shawnrannan@gmail.com>
2022-05-14 01:31:24 +0000

    Fix task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,15 +15,16 @@ int main(int argc, char *argv[])
 int num1, num2;
 char *op;
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-op = argv[2];
 if (argc != 4)
 {
 	printf("Error\n");
 	exit(98);
 }
-	if ((*op == '/' && num2 == 0) ||
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
+
+if ((*op == '/' && num2 == 0) ||
 	    (*op == '%' && num2 == 0))
 {
 	printf("Error\n");
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,7 +9,7 @@
 */
 int op_add(int a, int b)
 {
-	return ((a) + (b));
+	return (a + b);
 }
 /**
 * op_sub - returns the difference of a and b
@@ -20,7 +20,7 @@ int op_add(int a, int b)
 */
 int op_sub(int a, int b)
 {
-	return ((a) - (b));
+	return (a - b);
 }
 
 /**
@@ -33,7 +33,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	return ((a) * (b));
+	return (a * b);
 }
 
 /**
@@ -46,7 +46,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return ((a) / (b));
+	return (a / b);
 }
 
 /**
@@ -59,5 +59,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	return ((a) % (b));
+	return (a % b);
 }
