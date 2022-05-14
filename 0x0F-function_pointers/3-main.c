28d7e1e10e60be2c2e5032c547138d3116ebb348
rsannan <shawnrannan@gmail.com>
2022-05-14 00:55:26 +0000

    Add task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: argument string
+*
+* Return: result if success 98, 99, 100 otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+int num1, num2;
+char *op;
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op = argv[2];
+if (argc != 4)
+{
+	printf("Error\n");
+	exit(98);
+}
+if ((num2 == 0) && (*op == '/' || *op == '%'))
+{
+	printf("Error\n");
+	exit(100);
+}
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+	printf("Error\n");
+	exit(99);
+}
+printf("%d\n", get_op_func(op)(num1, num2));
+
+
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+* op_add - returns the sum of a and b
+* @a: first integer
+* @b: first integer
+*
+* Return: result
+*/
+int op_add(int a, int b)
+{
+	return ((a) + (b));
+}
+/**
+* op_sub - returns the difference of a and b
+* @a: first integer
+* @b: first integer
+*
+* Return: result
+*/
+int op_sub(int a, int b)
+{
+	return ((a) - (b));
+}
+
+/**
+* op_mul - returns the product of a and b
+* @a: first integer
+* @b: first integer
+*
+* Return: result
+*/
+
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+
+/**
+* op_div - returns the result of the division of a by b
+* @a: first integer
+* @b: first integer
+*
+* Return: result
+*/
+
+int op_div(int a, int b)
+{
+	return ((a) / (b));
+}
+
+/**
+* op_mod - returns the remainder of the division of a by b
+* @a: first integer
+* @b: first integer
+*
+*Return: result
+*/
+
+int op_mod(int a, int b)
+{
+	return ((a) % (b));
+}
