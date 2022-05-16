b203c588e8b135f25fa978aa8e9c2f22934361bd
rsannan <shawnrannan@gmail.com>
2022-05-16 16:47:39 +0000

    Fix task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,14 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-typedef char c;
-typedef int i;
-typedef float f;
-typedef char* s;
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 #endif
