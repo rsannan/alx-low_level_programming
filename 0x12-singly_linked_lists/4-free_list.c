7160624974cb68be60e78746cfe55aa1e656d207
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-05-24 20:10:24 +0000

    Add task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+* free_list - prints all elements in a linked list
+* @head: pointer to first element in list
+*
+* Return: void
+*/
+void free_list(list_t *head)
+{
+int state = 0;
+list_t *now = head;
+list_t *pre_link;
+while (state == 0)
+{
+	pre_link = now;
+	now = now->next;
+	if (now->next == NULL)
+		state = 1;
+	free(pre_link->str);
+	free(pre_link);
+}
+free(now->str);
+free(now);
+return;
+}
