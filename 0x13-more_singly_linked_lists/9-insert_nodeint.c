0ffce32450e382eb317635e259498cefdbb08a84
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-05-27 00:12:12 +0000

    Fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,37 +11,32 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int count = 0, state = 1;
-listint_t *temp = *head;
-listint_t *index;
-listint_t *newnode = (listint_t *)malloc(sizeof(listint_t));
-if (newnode == NULL)
-	return (NULL);
-newnode->n = n;
-while (temp)
-{
-	if (count != (idx - 1) && temp->next != NULL)
+listint_t *new, *copy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
 	{
-		count++;
-		temp = temp->next;
-	}
-	else
-	{
-		state = 0;
-		break;
+		new->next = copy;
+		*head = new;
+		return (new);
 	}
 
-}
-if (state == 1)
-{
-	return (NULL);
-}
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
 
-else if (state == 0)
-{
-	index = temp->next;
-	temp->next = newnode;
-	newnode->next = index;
-}
-return (newnode);
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
 }
