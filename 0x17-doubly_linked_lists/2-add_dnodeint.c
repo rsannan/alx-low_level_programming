177517d5253589e89f40f61293729a1839c5f0f0
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-07 15:56:03 +0000

    fix task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->n = n;
 	new->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
 	*head = new;
 
 	return (new);
