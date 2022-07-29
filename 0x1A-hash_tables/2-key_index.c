992ca48cf8a0ec0acc51ebc9c78faa14411dbf79
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-29 14:47:28 +0000

    Add task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+* key_index - gives the index of a key
+* @key: the key in question
+* @size: the size of the has array
+
+* Return: index of key/value pair
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value;
+	if (key == NULL)
+		return (EXIT_FAILURE);
+
+	hash_value = hash_djb2(key) % size;
+
+	return (hash_value);
+}
