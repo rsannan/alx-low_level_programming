6be3cd8944657c6c3ae9f350655e315039eaf249
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-29 13:15:36 +0000

    Add task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,15 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
-	
+
 	if (new_table == NULL)
 		return (NULL);
-	
+
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	
+
 	if (new_table->array == NULL)
-		return NULL;
+		return 	(NULL);
 
 	for (i = 0; i < size; i++)
 	{
